d99e31d4f2eb05c1905030414fb95514dbcc1a8e
Caio Oliveira <cfoviana@alu.ufc.br>
2024-10-22 11:23:36 -0300

    feat: add products to home page

--- crash_course/src/app/home/home.component.ts
+++ crash_course/src/app/home/home.component.ts
@@ -1,21 +1,25 @@
 import { Component } from '@angular/core';
 import { ProductsService } from '../services/products.service';
+import { Product } from '../../types';
+import { ProductComponent } from '../components/product/product.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ProductComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
   constructor(private productsService: ProductsService) {}
 
+  products: Product[] = [];
   ngOnInit() {
     this.productsService
       .getProducts('http://localhost:3000/clothes', { page: 0, perPage: 5 })
       .subscribe((products) => {
-        console.log(products);
+        this.products = products.items;
       });
   }
 }
