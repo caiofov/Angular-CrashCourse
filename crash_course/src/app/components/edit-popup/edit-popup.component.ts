59f7d47b85d9e9b242618065e21b4b0423a8ca6d
Caio Oliveira <cfoviana@alu.ufc.br>
2024-10-22 16:19:55 -0300

    feat: form validation

--- crash_course/src/app/components/edit-popup/edit-popup.component.ts
+++ crash_course/src/app/components/edit-popup/edit-popup.component.ts
@@ -2,7 +2,14 @@ import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { DialogModule } from 'primeng/dialog';
 import { Product } from '../../../types';
-import { FormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 import { RatingModule } from 'primeng/rating';
 import { ButtonModule } from 'primeng/button';
 
@@ -15,11 +22,24 @@ import { ButtonModule } from 'primeng/button';
     FormsModule,
     RatingModule,
     ButtonModule,
+    ReactiveFormsModule,
   ],
   templateUrl: './edit-popup.component.html',
   styleUrl: './edit-popup.component.scss',
 })
 export class EditPopupComponent {
+  productForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.productForm = this.formBuilder.group({
+      id: [0, [Validators.required]],
+      name: ['', [Validators.required, this.specialCharacterValidator()]],
+      image: ['', [Validators.required]],
+      price: ['', [Validators.required]],
+      rating: [0, [Validators.required]],
+    });
+  }
+
   @Input() display: boolean = false;
   @Output() displayChange = new EventEmitter<boolean>();
 
@@ -34,11 +54,25 @@ export class EditPopupComponent {
 
   @Output() confirm = new EventEmitter<Product>();
 
+  specialCharacterValidator(): ValidatorFn {
+    return (control) => {
+      const hasSpecialCharacter = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/.test(
+        control.value
+      );
+
+      return hasSpecialCharacter ? { hasSpecialCharacter: true } : null;
+    };
+  }
+
   onConfirm() {
-    this.confirm.emit(this.product);
+    this.confirm.emit(this.productForm.value as Product);
   }
 
   onCancel() {
     this.display = false;
+  }
+
+  ngOnChanges() {
+    this.productForm.patchValue(this.product);
   }
 }
