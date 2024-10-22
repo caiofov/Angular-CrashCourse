76311d9f288460a259101b22908b1f14f6ed13d8
Caio Oliveira <cfoviana@alu.ufc.br>
2024-10-22 13:31:10 -0300

    feat: update home with new features and add product

--- crash_course/src/app/home/home.component.ts
+++ crash_course/src/app/home/home.component.ts
@@ -4,10 +4,18 @@ import { Product } from '../../types';
 import { ProductComponent } from '../components/product/product.component';
 import { CommonModule } from '@angular/common';
 import { PaginatorModule } from 'primeng/paginator';
+import { EditPopupComponent } from '../components/edit-popup/edit-popup.component';
+import { ButtonModule } from 'primeng/button';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ProductComponent, CommonModule, PaginatorModule],
+  imports: [
+    ProductComponent,
+    CommonModule,
+    PaginatorModule,
+    EditPopupComponent,
+    ButtonModule,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
@@ -17,6 +25,37 @@ export class HomeComponent {
   products: Product[] = [];
   totalRecords: number = 0;
   rows: number = 5;
+
+  displayEditPopup: boolean = false;
+  displayAddPopup: boolean = false;
+
+  selectedProduct: Product = {
+    id: 0,
+    name: '',
+    image: '',
+    price: '',
+    rating: 0,
+  };
+
+  onConfirmAdd(product: Product) {
+    this.addProduct(product);
+    this.displayAddPopup = false;
+  }
+  onConfirmEdit(product: Product) {
+    this.editProduct(product);
+    this.displayEditPopup = false;
+  }
+
+  toggleEditPopup(product: Product) {
+    this.selectedProduct = product;
+    this.displayEditPopup = true;
+  }
+  toggleAddPopup() {
+    this.displayAddPopup = true;
+  }
+  toggleDeletePopup(product: Product) {
+    this.deleteProduct(product);
+  }
 
   onPageChange(event: any) {
     this.fetchProducts(event.page, event.rows);
